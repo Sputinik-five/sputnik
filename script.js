4dc9c79f642d5491c59da9635bb7a73734ce1324
Matheus Fontinele <fontinele9625@gmail.com>
2023-01-30 21:37:10 -0300

    :tada initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    if(html.classList.contains('light')) {
+        
+    } else {
+        
+    }
+    
+    // pegar a tag img
+    // se tiver light mode, adicionar a imagem light
+    // substituir a imagem
+    // se tiver sem light mode, manter a imagem normal
+}
